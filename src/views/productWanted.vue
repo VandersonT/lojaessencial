d6c1e997f5bebfcc4374a8666d61e1a402a38586
VandersonThiago <vandersontpaulo@gmail.com>
2021-11-10 11:14:55 -0300

    Finalizado o layout da página de exibição dos produtos procurados

--- src/views/productWanted.vue
+++ src/views/productWanted.vue
@@ -52,8 +52,45 @@
                 <button>Filtrar</button>
 
             </div>
-            <div>
-                Resultado para {{$route.params.wanted}}
+            <div class="boxResults">
+                <h1>Resultado para {{$route.params.wanted}}:</h1>
+                <div class="showCase">
+                    <div class="productSingle">
+                        <img src="https://images.tcdn.com.br/img/img_prod/482949/blusa_feminina_minuty_country_bordada_275_2764_1_20210809100307.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="https://images.tcdn.com.br/img/img_prod/482949/blusa_feminina_minuty_country_suede_291_2778_1_20210809101902.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="http://2.bp.blogspot.com/-ikgwp4F21S4/Ue8bTIXrw-I/AAAAAAAAAt0/Bsu-z3sDLvE/s1600/Blusa_de_Manga_L_4f146ae67a4b9.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="https://institucional.lojasleader.com.br/wp-content/uploads/2019/11/BLUSA-CROPPED-ESTAMPA-JOIAS-5999.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="https://www.gsuplementos.com.br/upload/produto/imagem/b_cal-a-jogger-growth-manuscrito.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Imagem-de-Sapato-em-PNG-1280x720.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                    <div class="productSingle">
+                        <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/04/Rel%C3%B3gio-Sony-PNG.png" />
+                        <p class="productDescription">loren ipsun dolor amet sit test, so i want to eat a piece of meat today</p>
+                        <p class="productPrice">R$ 999,00</p>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
@@ -127,4 +164,18 @@
         text-transform: uppercase;
         cursor: pointer;
     }
+
+    .boxResults{
+        width: calc(100vw - 350px);
+        padding: 20px;
+    }
+    .boxResults h1{
+        color: #858585;
+        font-family: Arial,tahoma,verdana;
+        font-weight: lighter;
+        margin-top: 10px;
+        margin-bottom: 20px;
+    }
+    /*A página que exibe os produtos que foram encontrados usa a mesma estrutura html/css que a página home: .
+    Então, para não repetir css o css dele é compartilhado e se encontra em style da página home*/
 </style>
